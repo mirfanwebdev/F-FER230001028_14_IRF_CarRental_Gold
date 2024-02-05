b06e81d1bf8d0b5a0e1c18aabd46d1d1b9f8f66f
m.irfan <m.irfanwebdev@gmail.com>
2024-02-05 12:50:59 +0700

    fix format and responsive

--- src/components/CarList/index.jsx
+++ src/components/CarList/index.jsx
@@ -1,23 +1,35 @@
-import { Link } from 'react-router-dom';
-import './styles.css'
+import { Link } from "react-router-dom";
+import "./styles.css";
 
 function CarList({ cars }) {
-    
-    return (
-        <div className="car-list-container">
-            {cars.map((item) => (
-                <div key={item.id} className="car-card">
-                    <img src={item.image} />
-                    <p>{item.name}</p>
-                    <h2>Rp. {item.price}/ hari</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                    <Link to={`/search/${item.id}`}>
-                        <button>Pilih Mobil</button>
-                    </Link>
-                </div>
-            ))}
+  const NOTFOUND =
+    "https://placehold.jp/30/c9c9c9/ffffff/300x150.png?text=image+notfound";
+  const formatedPrice = (price) => {
+    return new Intl.NumberFormat("id-ID", {
+      style: "currency",
+      currency: "IDR",
+    }).format(price);
+  };
+
+  return (
+    <div className="car-list-container">
+      {cars.map((item) => (
+        <div key={item.id} className="car-card">
+          {item.image ? <img src={item.image} /> : <img src={NOTFOUND} />}
+          {/* <img src={item.image} /> */}
+          <p>{item.name}</p>
+          <h2>{formatedPrice(item.price)}/ hari</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+          <Link to={`/search/${item.id}`}>
+            <button>Pilih Mobil</button>
+          </Link>
         </div>
-    )
+      ))}
+    </div>
+  );
 }
 
 export default CarList;
